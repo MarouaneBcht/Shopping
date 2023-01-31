a348cbed3e910564c55b0fc617a58ba93a90772f
MarouaneBcht <mrnb778@gmail.com>
2023-01-31 22:52:45 +0100

    Ajoutez des fichiers projet.

---
+++ Shopping/Migrations/20230121225543_Second.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Occupation",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Occupation",
+                table: "AspNetUsers");
+        }
+    }
+}
